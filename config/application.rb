d25912355175186468b7ff787c1be6b91d28fba6
Linch1 <m.carassiti@hotmail.it>
2022-06-01 12:01:50 +0200

    aded missing config

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Mongoid.load!("../mongoid.yml")
+Mongoid.load!("mongoid.yml")
 
 module Communitywork
   class Application < Rails::Application
