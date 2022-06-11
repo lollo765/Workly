861d0ee76bafb3343249be47aba1d274ee5225b2
lollo765 <marcmat76@gmail.com>
2022-06-11 18:19:50 +0200

    aggiunta classe lavoro

---
+++ app/models/lavoro.rb
@@ -0,0 +1,25 @@
+class Lavoro
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :title, type: String
+  field :description, type: String
+  field :n_Lavoratori, type: Integer
+  field :categories, type: String
+  field :skill_Lavoratori, type: String
+  field :timer, type:Integer
+  field :currency, type: String
+  field :candidates, type: Array
+
+  validates :title, :presence => true, uniqueness: { case_sensitive: false}
+  validates :description, :presence => true, length: {minimum: 10}
+  validates :n_Lavoratori, :presence => true
+  validates :categories, :presence => true
+  validates :skill_Lavoratori, :presence => true
+
+  def self.search(input) 
+    if input
+        any_of({title: /#{input}/i}, {description: /#{input}/i}, {categories: /#{input}/i}, {skill_Lavoratori: /#{input}/i})
+    end
+  end
+
+end
